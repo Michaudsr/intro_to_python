a811a55ff28ab4bd4b22152ab93ceda23eed03ee
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-09-14 18:36:14 -0400

    [car] added car class

---
+++ intro_to_python/car.py
@@ -0,0 +1,12 @@
+class car():
+  def __init__(self, kind):
+    self.model = silverado
+    self.brand = chevrolet
+    self.year = 2016
+
+def drive(self, miles):
+  self.millage += miles
+
+def fuel(self, gallons):
+  self.gallons = 22
+  
